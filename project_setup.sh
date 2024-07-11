6235b8cbffe86e1046cffb2cbcc0416b7bfb7115
Matteo <matteo@MacBook-Air-di-Matteo.local>
2024-07-11 20:03:51 +0200

    aggiunto run file

--- project_setup.sh
+++ project_setup.sh
@@ -1,13 +1,10 @@
 mkdir airflow
 
 pip3 install virtualenv -v
-python3 -m virtualenv project_env
-source ./project_env/bin/activate           
-
+python3 -m virtualenv project_env       
+source ./project_env/bin/activate    
 pip install -r requirements.txt
 
-#remove examples dags
-rm project_env/lib/python3.9/site-packages/airflow/example_dags/*.py
 
 #setting up environment variables
 DAGS_DIR="$(pwd)/dags"
@@ -15,5 +12,8 @@ AIRFLOW_DIR="$(pwd)/airflow"
 export AIRFLOW_HOME=$AIRFLOW_DIR
 export AIRFLOW__CORE__DAGS_FOLDER=$DAGS_DIR
 
-#run airflow in dev mode
-airflow standalone
+
+#remove examples dags
+rm project_env/lib/python3.9/site-packages/airflow/example_dags/*.py
+
+deactivate
---
+++ run.sh
@@ -0,0 +1,3 @@
+source ./project_env/bin/activate  
+#run airflow in dev mode
+airflow standalone
